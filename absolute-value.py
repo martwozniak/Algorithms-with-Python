f2c124a057a033ccd011191eded376b20a226577
Martin <persky03@gmail.com>
2020-12-03 07:21:43 +0100

    first commit

---
+++ absolute-value.py
@@ -0,0 +1,9 @@
+# User input data
+x = input("Please, give x value")
+
+# Check if given data is bigger or smaller than 0
+if int(x) >= 0: # If x is bigger than 0
+    print(x) # We printing, because we are outside the function
+else: # If not
+    result = - int(x) # - Change to opposite number
+    print(result) # We printing, because we are outside the function
